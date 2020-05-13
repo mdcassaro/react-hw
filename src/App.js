4c30d2ceadcc2496e72f4d6ba256baf769230857
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-13 13:24:48 -0500

    updated wrapper in app js for json data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import employees from "./employees.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    employees
+  };
+
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ friends });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>employees List</Title>
+        {this.state.employees.map(employees => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={employees.id}
+            key={employees.id}
+            name={employees.name}
+            occupation={employees.occupation}
+            email={employees.email}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
