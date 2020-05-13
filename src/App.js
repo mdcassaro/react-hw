404bea5f73542b5743def39decbb548468734a02
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-13 16:09:05 -0500

    search bar added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import employees from "./employees.json";
+import EmployeeSearch from "./components/EmployeeSearch";
 
 class App extends Component {
  
@@ -17,13 +18,41 @@ class App extends Component {
     this.setState({ employees });
   };
 
+  sortEmployees = (name) =>{
+    const sortedEmployees = this.state.employees.filter(employee => employee.name);
+    this.setState({ sortedEmployees })
+  }
+
+  
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // When the form is submitted, search the OMDB API for the value of `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.sortEmployees(this.state.search);
+  };
+  
+  
+
   render() {
     return (
       <Wrapper>
         <Title>Employee's List</Title>
+        <EmployeeSearch
+                value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+              />
         {this.state.employees.map(employees => (
           <EmployeeCard
             removeEmployee={this.removeEmployee}
+            sortEmployees={this.sortEmployees}
             id={employees.id}
             key={employees.id}
             name={employees.name}
