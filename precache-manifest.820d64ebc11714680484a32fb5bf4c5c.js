2ebfbc73d897000fbce982f97331c2763f44b44c
Michael Cassaro <mdcassaro@hotmail.com>
2020-05-27 13:50:05 -0500

    Updates

---
+++ precache-manifest.820d64ebc11714680484a32fb5bf4c5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afaae2a5cbc11d45fa2e443a2a1da80",
+    "url": "/reacthw/index.html"
+  },
+  {
+    "revision": "7f5ae7307875bd7d75c2",
+    "url": "/reacthw/static/css/main.2cc5d61a.chunk.css"
+  },
+  {
+    "revision": "ba6285ca736e0d9992cb",
+    "url": "/reacthw/static/js/2.67f086ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacthw/static/js/2.67f086ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5ae7307875bd7d75c2",
+    "url": "/reacthw/static/js/main.2dc101f0.chunk.js"
+  },
+  {
+    "revision": "7b97fd7e7d6d8f91ae16",
+    "url": "/reacthw/static/js/runtime-main.792d9e54.js"
+  }
+]);
